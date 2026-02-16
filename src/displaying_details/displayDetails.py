9e3bfd9af09d80f45b35dc1b56b69a4bc3d0b8e4
Vishnu M <vishnuofficial004@gmail.com>
2026-02-16 13:15:31 +0530

    feat:added embedding extraction

--- src/displaying_details/displayDetails.py
+++ src/displaying_details/displayDetails.py
@@ -4,3 +4,12 @@ import torch
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load("ViT-B/32", device=device)
 print(model)
+
+
+# Function to Convert ROI to Image
+def get_image_embedding(image_path):
+    image = preprocess(Image.open(image_path)).unsqueeze(0).to(device)
+    with torch.no_grad():
+        embedding = model.encode_image(image)
+        embedding = embedding / embedding.norm(dim=-1, keepdim=True)
+    return embedding.cpu().numpy()
